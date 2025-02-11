877df581769d6b486ebdf2a890bc236e484f58c2
Ghulam Ahmed <noorghulam7153@gmail.com>
2025-02-10 21:13:39 -0500

    uploading project to github

---
+++ app/src/main/java/com/example/cs388lab2/Email.kt
@@ -0,0 +1,7 @@
+package com.example.cs388lab2
+
+class Email(
+    val sender:String,
+    val title: String,
+    val summary: String) {
+}
---
+++ app/src/main/java/com/example/cs388lab2/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.cs388lab2
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var emails: List<Email>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Lookup the RecyclerView in activity layout
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
+            // Fetch next 5 emails and display in RecyclerView
+            val newEmails = EmailFetcher.getNext5Emails()
+            (emails as MutableList<Email>).addAll(newEmails)
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
